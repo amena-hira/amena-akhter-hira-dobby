cdb9ac1d36c0b39ba06ad4991a4179376d7fe1d1
Amena Akhter Hira <amena35-2139@diu.edu.bd>
2023-05-11 01:36:37 +0200

    show images

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import ImageForm from './ImageForm';
 import ImageGallery from './ImageGallery';
 
 const Home = () => {
+    const [images, setImages] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/image')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setImages(data);
+            })
+    }, [])
     return (
         <>
             <div className='bg-sky-300'>
                 <Navbar></Navbar>
             </div>
             <div className='max-w-7xl mx-auto my-10 px-2 grid justify-items-center grid-cols-1 lg:grid-cols-2 gap-2'>
-                <ImageForm></ImageForm>
-                <ImageGallery></ImageGallery>
+                <ImageForm setImages={setImages}></ImageForm>
+                <ImageGallery images={images}></ImageGallery>
             </div>
 
         </>
--- src/Pages/Home/ImageForm.js
+++ src/Pages/Home/ImageForm.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-const ImageForm = () => {
+const ImageForm = ({ setImages }) => {
     const [nameValue, setNameValue] = useState(false);
     const [imagesValue, setImagesValue] = useState(false);
-    const handleUploadImage = (event) =>{
+    const handleUploadImage = (event) => {
         event.preventDefault();
         const form = event.target;
         const name = form.name.value;
@@ -11,24 +11,31 @@ const ImageForm = () => {
         console.log(name, image);
         const formData = new FormData();
         formData.append('name', name);
-        formData.append('image',image);
+        formData.append('image', image);
         fetch('http://localhost:5000/image', {
-                method: 'POST',
-                body:formData
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(data => {
+                fetch('http://localhost:5000/image')
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        setImages(data);
+                    })
             })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
+            .catch(error => console.log(error))
     }
     return (
-        <>
+        <div>
             <div className="card max-w-sm lg:max-w-lg shadow-2xl bg-base-100">
                 <form className="card-body" onSubmit={handleUploadImage}>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
-                        <input type="text" name='name' onChange={()=>setNameValue(true)} placeholder="Name" className="input input-bordered" />
+                        <input type="text" name='name' onChange={() => setNameValue(true)} placeholder="Name" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
@@ -40,22 +47,22 @@ const ImageForm = () => {
                             </div>
                             <label className="block">
                                 <span className="sr-only">Choose profile photo</span>
-                                <input type="file" name='image' onChange={()=>setImagesValue(true)} className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100" />
+                                <input type="file" name='image' onChange={() => setImagesValue(true)} className="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100" />
                             </label>
                         </div>
                     </div>
                     <div className="form-control mt-6">
                         {
                             nameValue && imagesValue ?
-                            <button className="btn bg-sky-300 border-none hover:bg-sky-900">Submit</button>
-                            :
-                            <button className="px-8 py-3 text-white bg-sky-300 rounded focus:outline-none" disabled>Submit</button>
+                                <button className="btn bg-sky-300 border-none hover:bg-sky-900">Submit</button>
+                                :
+                                <button className="px-8 py-3 text-white bg-sky-300 rounded focus:outline-none" disabled>Submit</button>
                         }
                     </div>
                 </form>
             </div>
 
-        </>
+        </div>
     );
 };
 
